6e5b09cebec89ee4f10567643009db399de8e25e
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-05-04 23:36:25 +0530

    Comit-982

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
-variable "region" {
-  default = "ap-south-1"
+variable "aws_region" {
+  description = "AWS region"
+  default     = "ap-south-1"
+}
+
+variable "bucket_name" {
+  description = "Globally unique S3 bucket name"
 }
